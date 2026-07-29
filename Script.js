70ff6b2b7fc0ccc0f4ca7e7fe01ef442a80695e4
mcbattle.jp <tsu.otk.mcd@gmail.com>
2026-07-29 12:26:42 +0900

    Script.js を更新

--- Script.js
+++ Script.js
@@ -1,1 +1,31 @@
+// 夜敷 Official Website
+
+const year = document.querySelector("footer p");
+
+if (year) {
+  const currentYear = new Date().getFullYear();
+  year.innerHTML = `&copy; ${currentYear} 夜敷`;
+}
+
+// スムーズスクロール（古いブラウザ対策）
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", event => {
+    const targetId = link.getAttribute("href");
+
+    if (targetId === "#") return;
+
+    const target = document.querySelector(targetId);
+
+    if (!target) return;
+
+    event.preventDefault();
+
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  });
+});
+
+console.log("Yorushiki Official Website Loaded");
 
